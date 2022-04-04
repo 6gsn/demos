89a65bf9562da2b6fa451c8923575f0228b07c96
park.byeongseon <park.byeongseon@linecorp.com>
2022-04-04 16:48:04 +0900

    udpate ga to gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,9 +41,12 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: "gatsby-plugin-google-gtag",
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingIds: [process.env.GOOGLE_ANALYTICS_ID],
+        pluginConfig: {
+          head: true,
+        },
       },
     },
     `gatsby-plugin-sitemap`,
