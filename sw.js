62f4016c6df76a54f5fec8c5494bd8b6d011c4c3
park.byeongseon <park.byeongseon@linecorp.com>
2022-04-07 17:50:35 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1b6da83ba52ca9ff66fc.js"
+    "url": "webpack-runtime-6b5c7670b121f3b1180b.js"
   },
   {
     "url": "framework-bd73b6d0078bd171570d.js"
   },
   {
-    "url": "app-9862e7a03923782fd1aa.js"
+    "url": "app-b98f9100cf7b20f9b4d5.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3fb196c63dcf26e1c011bd091ecf748a"
+    "revision": "1d0f393d1042a233f4278fbdc567a759"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-4e6d71cb47fc4b206789.js"
@@ -56,7 +56,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "ec1686283f7cce8d7cb1194c6066493e"
+    "revision": "ffe74b37fc945924d94f0f2381827581"
   },
   {
     "url": "polyfill-fd1b2aba5de6a62a44db.js"
@@ -155,7 +155,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/demos/app-9862e7a03923782fd1aa.js`))) {
+  if (!resources || !(await caches.match(`/demos/app-b98f9100cf7b20f9b4d5.js`))) {
     return await fetch(event.request)
   }
 
